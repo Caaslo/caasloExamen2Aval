ae6e619a2086451d379f600dd39a071d923fd45a
Carlos Asensi López <114494804+Caaslo@users.noreply.github.com>
2023-02-14 16:14:07 +0100

    script e y correccion del c

--- bminymax.sh
+++ bminymax.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readarray -t consumos < consumos.txt
+readarray -t consumos < <(tail -n +2 consumos.txt)
 
 min_consumo=999999
 max_consumo=0
@@ -11,22 +11,22 @@ min_fecha=""
 max_fecha=""
 
 for ((i=0; i<${#consumos[@]}; i++)); do
-ciudad=$(echo ${consumos[$i]} | awk '{print $1}')
-mes=$(echo ${consumos[$i]} | awk '{print $2}')
-anio=$(echo ${consumos[$i]} | awk '{print $3}')
-consumo=$(echo ${consumos[$i]} | awk '{print $4}')
+    ciudad=$(echo ${consumos[$i]} | awk '{print $1}')
+    mes=$(echo ${consumos[$i]} | awk '{print $2}')
+    anio=$(echo ${consumos[$i]} | awk '{print $3}')
+    consumo=$(echo ${consumos[$i]} | awk '{print $4}')
 
-if (( $consumo > $max_consumo )); then
-    max_consumo=$consumo
-    max_ciudad=$ciudad
-    max_fecha="$mes $anio"
-fi
+    if (( $consumo > $max_consumo )); then
+        max_consumo=$consumo
+        max_ciudad=$ciudad
+        max_fecha="$mes $anio"
+    fi
 
-if (( $consumo < $min_consumo )); then
-    min_consumo=$consumo
-    min_ciudad=$ciudad
-    min_fecha="$mes $anio"
-fi
+    if (( $consumo < $min_consumo )); then
+        min_consumo=$consumo
+        min_ciudad=$ciudad
+        min_fecha="$mes $anio"
+    fi
 done
 
 echo "El consumo mínimo es: $min_ciudad,$min_fecha, $min_consumo"
